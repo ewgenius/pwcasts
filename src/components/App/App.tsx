8ff5fd876d3df8e45cd2578fda576b134784f2d8
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-03-03 23:20:28 +0300

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Component } from 'react';
+import './App.scss';
+
+class App extends Component<any, any> {
+  render() {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <h2>Welcome to React</h2>
+        </div>
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
