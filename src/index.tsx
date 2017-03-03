dd75eb943d437260c0f51b5d81677c54aca5e327
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-03-04 00:45:50 +0300

    upd

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,18 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App/App';
 
+const sw = require('file-loader?name=sw.js!ts-loader!./sw.ts');
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register(sw)
+    .then(registration => {
+      console.log(registration);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
